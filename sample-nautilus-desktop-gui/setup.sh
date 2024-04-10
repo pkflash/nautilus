fdc4603f62a1d140b299f2ffcd9a56abe12633c7
Jonathan Morris <jonathan_wellington@icloud.com>
2024-04-10 10:25:39 -0700

    Yes

--- sample-nautilus-desktop-gui/setup.sh
+++ sample-nautilus-desktop-gui/setup.sh
@@ -1,10 +1,13 @@
 NAME=j-desktop
+CACHE_NAME=j-carla-cache
 
 if [ "$1" -eq 1 ]; then
     kubectl delete deployment $NAME
     kubectl delete service $NAME
     kubectl delete ingress $NAME
 elif [ "$1" -eq 0 ]; then
+    kubectl create -f storage.yml
+    kubectl create -f cache.yml
     kubectl create -f desktop.yml
     kubectl create -f desktop-ingress.yml
 else
